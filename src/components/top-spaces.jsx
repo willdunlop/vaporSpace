fc68a1095ea27835cfe5be58893da312ea9a001b
William Dunlop <william.dunlop.1993@gmail.com>
2017-12-16 15:11:45 +1100

    improved style and design

--- src/components/top-spaces.jsx
+++ src/components/top-spaces.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const TopSpaces = () => (
-  <div className="top-spaces">
-    <h2 className="top-spaces__title">
+  <div className="ranked-spaces">
+    <h2 className="ranked-spaces__title">
       S p a c e s
     </h2>
 
-    <div className="top-spaces__top">
-      <p className="top-spaces__top-title">Top</p>
+    <div className="ranked-spaces__section">
+      <p className="ranked-spaces__sub-title">Top</p>
       <hr />
-      <ol className="top-spaces__top-content">
+      <ol className="ranked-spaces__top">
         <li>~AcquiredTaste</li>
         <li>~AcquiredTaste</li>
         <li>~AcquiredTaste</li>
@@ -24,8 +24,15 @@ const TopSpaces = () => (
       </ol>
     </div>
 
-    <div className="top-spaces__random">
-      
+
+    <div className="ranked-spaces__section">
+      <p className="ranked-spaces__sub-title">Random</p>
+      <hr />
+      <ul className="ranked-spaces__random">
+        <li className="ranked-spaces__random-item">
+          
+        </li>
+      </ul>
     </div>
 
   </div>
